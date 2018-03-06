4d574e1f3b5e8b469be5dd69ab6649c71d8c7bc3
Wizard <715759898@qq.com>
2018-03-06 23:50:41 +0800

    login

--- src/main/java/chapter2/homework/gdsf.java
+++ src/main/java/chapter2/homework/gdsf.java
@@ -1,19 +1,20 @@
-package chapter2;
+package chapter2.homework;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-public class Demo2Servlet extends HttpServlet {
+
+@WebServlet(name = "/test",displayName="/test",urlPatterns = "/test")
+public class gdsf extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ServletContext servletContext = this.getServletContext();
-        Object object = servletContext.getAttribute("username");
-        System.out.println(object);
+        super.doPost(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        System.out.println("我被访问到了一次哟");
     }
+
 }
